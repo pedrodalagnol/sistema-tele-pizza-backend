c09fb36c066e98460951d2959e3f66dbfad0bcd6
pedrodalagnol <joao.rossatti@edu.pucrs.br>
2025-09-24 22:35:22 -0300

    first commit

---
+++ src/main/java/com/grupo11/sistema_tele_pizza_backend/SistemaTelePizzaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grupo11.sistema_tele_pizza_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTelePizzaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTelePizzaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo11/sistema_tele_pizza_backend/SistemaTelePizzaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo11.sistema_tele_pizza_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTelePizzaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
